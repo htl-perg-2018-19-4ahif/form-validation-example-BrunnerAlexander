e1fbd5b7575041ecab7a6a1273b7211f3b4dd725
Alexander Brunner <AlexanderBrunner01@gmail.com>
2018-11-15 13:31:48 +0100

    02_Brunner_PartSolution2

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 $(document).on("load change", function () {
+    var submit = true;
     if ($("#firstName").val() == "") {
         $("#firstNameMandatory").show();
+        submit = false;
     }
     else {
         $("#firstNameMandatory").hide();
     }
     if ($("#lastName").val() == "") {
         $("#lastNameMandatory").show();
+        submit = false;
     }
     else {
         $("#lastNameMandatory").hide();
     }
     if ($("#email").val() == "") {
         $("#emailMandatory").show();
+        submit = false;
     }
     else {
         $("#emailMandatory").hide();
+    }
+    if (submit == false) {
+        $("#sub").hide();
+    }
+    else {
+        $("#sub").show();
     }
 });
--- app.ts
+++ app.ts
@@ -1,19 +1,30 @@
 $(document).on("load change", function(){
+    let submit:boolean = true;
+    
     if($("#firstName").val() == ""){
         $("#firstNameMandatory").show();
+        submit = false;
     }else{
         $("#firstNameMandatory").hide();
     }
 
     if($("#lastName").val() == ""){
         $("#lastNameMandatory").show();
+        submit = false;
     }else{
         $("#lastNameMandatory").hide();
     }
 
     if($("#email").val() == ""){
         $("#emailMandatory").show();
+        submit = false;
     }else{
         $("#emailMandatory").hide();
+    }
+
+    if(submit == false){
+        $("#sub").hide();
+    }else{
+        $("#sub").show();
     }
 })
